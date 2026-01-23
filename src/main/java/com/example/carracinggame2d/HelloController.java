b059ccf16a82fdc5694c004478d7642b35df606e
thinJuno <thinlam22@gmail.com>
2026-01-23 22:01:11 +0700

    new game

---
+++ src/main/java/com/example/carracinggame2d/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.carracinggame2d;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.example.carracinggame2d {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    exports com.carracinggame;
+}
